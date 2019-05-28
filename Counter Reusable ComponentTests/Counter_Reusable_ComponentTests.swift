dc823c37b34d4c636a8e37fedf7cc06a57b82a89
Gupta, Rupali (US - Bengaluru) <rupgupta@usblrrupguptamb.local>
2019-05-28 12:32:50 +0530

    Initial Commit

---
+++ Counter Reusable ComponentTests/Counter_Reusable_ComponentTests.swift
@@ -0,0 +1,34 @@
+//
+//  Counter_Reusable_ComponentTests.swift
+//  Counter Reusable ComponentTests
+//
+//  Created by Gupta, Rupali (US - Bengaluru) on 28/05/19.
+//  Copyright © 2019 Gupta, Rupali (US - Bengaluru). All rights reserved.
+//
+
+import XCTest
+@testable import Counter_Reusable_Component
+
+class Counter_Reusable_ComponentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
